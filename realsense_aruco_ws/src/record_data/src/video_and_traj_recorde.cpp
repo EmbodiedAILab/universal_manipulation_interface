84c32402b48a39b35c7fa1ed273e1424553da293
zhanghengsjtu <zhanghengsjtu@gmail.com>
2024-06-02 18:10:08 +0800

    waiting for new message

--- realsense_aruco_ws/src/record_data/src/video_and_traj_recorde.cpp
+++ realsense_aruco_ws/src/record_data/src/video_and_traj_recorde.cpp
@@ -13,6 +13,26 @@ public:
         egoCameraSub_ = nh_.subscribe(EGO_CAMERA_NAME + "/camera_color/image_raw", 1, &VideoAndTrajRecorder::egoCameraCallback, this);
         extCameraSub_ = nh_.subscribe(EXT_CAMERA_NAME + "/camera_color/image_raw", 1, &VideoAndTrajRecorder::extCameraCallback, this);
         arucoPoseSub_ = nh_.subscribe(ARUCO_RES_NAME, 1, &VideoAndTrajRecorder::arucoPoseCallback, this);
+        while (ros::ok() && egoCameraFlag_) {
+            ROS_ERROR("Waiting for ego camera");
+            ros::Duration(0.5).sleep();
+        }
+        while (ros::ok() && extCameraFlag_) {
+            ROS_ERROR("Waiting for ext camera");
+            ros::Duration(0.5).sleep();
+        }
+        while (ros::ok() && arucoPoseFlag_) {
+            ROS_ERROR("Waiting for aruco pose");
+            ros::Duration(0.5).sleep();
+        }
+        ROS_INFO("Ready to record video");
+        
+        ros::Rate loopRate(30);
+        while (ros::ok()) {
+            // record video
+            ros::spinOnce();
+            loopRate.sleep();
+        } 
     }
 
 private:
@@ -21,19 +41,33 @@ private:
     ros::Subscriber extCameraSub_;
     ros::Subscriber arucoPoseSub_;
 
+    bool egoCameraFlag_ = false;
+    bool extCameraFlag_ = false;
+    bool arucoPoseFlag_ = false;
+
     const string EGO_CAMERA_NAME;
     const string EXT_CAMERA_NAME;
     const string ARUCO_RES_NAME = "aruco_single/pose";
     
     // 回调函数
     void egoCameraCallback(const sensor_msgs::ImageConstPtr& imagePtr) {
-
+        
+        if (egoCameraFlag_ == false) {
+            egoCameraFlag_ = true;
+        }
     }
     void extCameraCallback(const sensor_msgs::ImageConstPtr& imagePtr) {
 
+        if (extCameraFlag_ == false) {
+            extCameraFlag_ = true;
+        }
     }
     void arucoPoseCallback(const aruco_msgs::MarkerConstPtr& posePtr) {
 
+
+        if (arucoPoseFlag_ == false) {
+            arucoPoseFlag_ = true;
+        }
     }
 };
 
