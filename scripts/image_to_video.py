9e33f3d0a043d7452ef056208df59767503bd57f
CharlesssX1 <171160276@qq.com>
2024-06-22 15:01:10 +0800

    modify image_to_video sort policy

--- scripts/image_to_video.py
+++ scripts/image_to_video.py
@@ -1,10 +1,19 @@
 import cv2
 import os
+import re
+
+def extract_image_number(image_name):
+    # 使用正则表达式提取图片名称中的序号
+    match = re.search(r'rgb_(\d+)_\w+\.png', image_name)
+    if match:
+        return int(match.group(1))
+    else:
+        return -1  # 如果未能匹配到序号，返回一个负数作为默认值
 
 def images_to_video(image_folder, output_video_file, fps):
     # 获取所有图片文件名并排序
     images = [img for img in os.listdir(image_folder) if img.endswith(".jpg") or img.endswith(".png")]
-    images.sort()
+    images.sort(key=lambda x: extract_image_number(x))  # 根据提取的序号进行排序
 
     # 读取第一张图片以获取帧的宽度和高度
     first_image_path = os.path.join(image_folder, images[0])
