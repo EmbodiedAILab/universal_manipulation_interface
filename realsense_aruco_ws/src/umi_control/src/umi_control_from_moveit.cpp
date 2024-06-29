bd9992ebd878477c531c054bd5c14068907be3da
zhanghengsjtu <zhanghengsjtu@gmail.com>
2024-06-29 16:20:13 +0800

    add umi_control_from_moveit ros node

---
+++ realsense_aruco_ws/src/umi_control/src/umi_control_from_moveit.cpp
@@ -0,0 +1,322 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+
+#include <Eigen/Eigen>
+#include <Eigen/Geometry>
+#include <Eigen/Core>
+
+#include <ros/ros.h>
+#include <tf2/convert.h>
+#include <ros/package.h>
+#include <tf2_eigen/tf2_eigen.h>
+#include <tf/transform_listener.h>
+#include <sensor_msgs/Joy.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/Twist.h>
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_interface/planning_interface.h>
+#include <moveit/robot_model_loader/robot_model_loader.h>
+
+#include <fstream>
+#include <iomanip>
+#include <sstream>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <boost/filesystem.hpp>
+#include <regex>
+#include <unordered_map>
+#include <thread>
+
+namespace fs = boost::filesystem;
+using namespace std;
+
+struct O3DELinkInfo
+{
+    string linkName;
+    double x;
+    double y;
+    double z;
+    double qw;
+    double qx;
+    double qy;
+    double qz;
+
+    void print()
+    {
+        cout << "link name " << this->linkName << endl;
+        cout << "x: " << this->x << endl;
+        cout << "y: " << this->y << endl;
+        cout << "z: " << this->z << endl;
+        cout << "qw: " << this->qw << endl;
+        cout << "qx: " << this->qx << endl;
+        cout << "qy: " << this->qy << endl;
+        cout << "qz: " << this->qz << endl
+             << endl;
+    }
+};
+
+class UmiMoveitController
+{
+public:
+    UmiMoveitController() : robotModelLoader_("robot_description"),
+                      robotModel_(robotModelLoader_.getModel()),
+                      robotState_(new robot_state::RobotState(robotModel_)),
+                      armJointModelGroup_(robotState_->getJointModelGroup("arm")),
+                      gripperJointModelGroup_(robotState_->getJointModelGroup("gripper")),
+                      armMoveGroup_("arm"),
+                      gripperMoveGroup_("gripper")
+    {
+        currentJointValues_.resize(armJointNumber_);
+        setupUDPServer();
+        baseFrame_ = armMoveGroup_.getPoseReferenceFrame();
+        eefFrame_ = armMoveGroup_.getEndEffectorLink();
+        jointNames_ = armMoveGroup_.getVariableNames();
+        linkWithGeos_ = robotModel_->getLinkModelNamesWithCollisionGeometry();
+        robotState_ = armMoveGroup_.getCurrentState();
+
+        robotStateSub_ = nh_.subscribe("joint_states", 1, &UmiMoveitController::jointStatesCallback, this);
+        ros::Duration(0.5).sleep();
+        while (ros::ok() && !jointStatesFlag_)
+        {
+            ROS_ERROR_STREAM("Waiting for joint states, and the topic name you specified is " << robotStateSub_.getTopic());
+            ros::Duration(1.0).sleep();
+        }
+
+        std::thread commandThread(&UmiMoveitController::CommandProcessor, this);
+        commandThread.detach();
+
+        // make sure robot is at home and the gripper is opened
+        armMoveGroup_.setNamedTarget("home");
+        armMoveGroup_.move();
+        gripperMoveGroup_.setNamedTarget("open");
+        gripperMoveGroup_.move();
+
+        // objectPose和placePose需要从O3DE获取，此处随机示意
+        Eigen::Affine3d objectMatrix;
+        Eigen::Affine3d retreatMatrix;
+        Eigen::Affine3d placeMatrix;
+        
+        while (ros::ok())
+        {
+            objectMatrix = robotState_->getFrameTransform(eefFrame_);
+            objectMatrix(2,3) -= 0.2;
+            retreatMatrix = objectMatrix;
+            retreatMatrix(2,3) += 0.1;
+            placeMatrix = objectMatrix;
+            placeMatrix(1,3) += 0.1;
+
+            geometry_msgs::Pose objectPose, retreatPose, placePose;
+            tf2::convert(objectMatrix, objectPose);
+            tf2::convert(retreatMatrix, retreatPose);
+            tf2::convert(placeMatrix, placePose);
+
+            armMoveGroup_.setPoseTarget(retreatPose);
+            armMoveGroup_.move();
+            ROS_INFO("Approach");
+
+            armMoveGroup_.setPoseTarget(objectPose);
+            armMoveGroup_.move();
+            ROS_INFO("Pregrasp");
+
+            // gripperMoveGroup_.setNamedTarget("close");
+            gripperMoveGroup_.setJointValueTarget(getGripperJointsFromWidth(0.02));
+            gripperMoveGroup_.move();
+            ROS_INFO("Grasp");
+
+            armMoveGroup_.setPoseTarget(retreatPose);
+            armMoveGroup_.move();
+            ROS_INFO("Retreat");
+
+            armMoveGroup_.setPoseTarget(placePose);
+            armMoveGroup_.move();
+            ROS_INFO("PrePlace");
+
+            gripperMoveGroup_.setNamedTarget("open");
+            gripperMoveGroup_.move();
+            ROS_INFO("Place");
+
+            armMoveGroup_.setNamedTarget("home");
+            armMoveGroup_.move();
+            ROS_INFO("Home");
+            // 接收物体的位置信息，然后规划机械臂到指定位置
+            // 同步信息的发布放在jointStateCallback里面了
+        }
+    }
+
+    void CommandProcessor()
+    {
+        std::unordered_map<char, std::string> keyToMessage = {
+            {'c', "START"},
+            {'s', "STOP"},
+            {'r', "RESET"}
+        };
+
+        while (true)
+        {
+            char key = getchar();
+            auto it = keyToMessage.find(key);
+            if (it != keyToMessage.end())
+            {
+                sendDataToClients(it->second);
+                ROS_INFO("%s sent", it->second.c_str());
+                std::this_thread::sleep_for(std::chrono::seconds(1)); // 1秒内只发送一次
+            }
+        }
+    }
+
+private:
+    // moveit variables
+    robot_model_loader::RobotModelLoader robotModelLoader_;
+    robot_model::RobotModelPtr robotModel_;
+    robot_state::RobotStatePtr robotState_;
+    const robot_state::JointModelGroup *armJointModelGroup_;
+    const robot_state::JointModelGroup *gripperJointModelGroup_;
+    moveit::planning_interface::MoveGroupInterface armMoveGroup_;
+    moveit::planning_interface::MoveGroupInterface gripperMoveGroup_;
+
+    // ros communication variables
+    ros::NodeHandle nh_;
+    ros::Subscriber robotStateSub_;
+    bool jointStatesFlag_ = false;
+    vector<double> currentJointValues_;         // 机械臂6个关节角度
+    sensor_msgs::JointState jointStatesMsgs_;   // 机器人最新的数据
+    const int armJointNumber_ = 6;              // 机械臂关节数量
+
+    // socket
+    int udpSocket_;
+    struct sockaddr_in serverAddr_;
+    struct sockaddr_in clientAddr_;
+    socklen_t clientAddrLen_ = sizeof(clientAddr_);
+    
+    string baseFrame_;
+    string eefFrame_;
+    vector<string> jointNames_;
+    vector<string> linkWithGeos_;
+    double currentLeftFingerJoint_;
+    double currentRightFingerJoint_;
+    const double maxWidth_ = 0.08;
+    const double minWidth_ = 0.0;
+
+    void setupUDPServer()   
+    {
+        udpSocket_ = socket(AF_INET, SOCK_DGRAM, 0);
+        if (udpSocket_ < 0)
+        {
+            perror("socket creation failed");
+            exit(EXIT_FAILURE);
+        }
+
+        int broadcastEnable = 1;
+        if (setsockopt(udpSocket_, SOL_SOCKET, SO_BROADCAST, &broadcastEnable, sizeof(broadcastEnable)) < 0)
+        {
+            perror("setsockopt(SO_BROADCAST) failed");
+            exit(EXIT_FAILURE);
+        }
+
+        memset(&serverAddr_, 0, sizeof(serverAddr_));
+        serverAddr_.sin_family = AF_INET;
+        serverAddr_.sin_addr.s_addr = inet_addr("10.78.114.255"); // Subnet broadcast address
+        serverAddr_.sin_port = htons(65433);
+
+        ROS_INFO("UDP server set up and broadcasting on port 65433");
+    }
+
+    void sendDataToClients(const std::string &data)
+    {
+        ssize_t sentBytes = sendto(udpSocket_, data.c_str(), data.size(), MSG_CONFIRM,
+                                (const struct sockaddr *)&serverAddr_, sizeof(serverAddr_));
+        if (sentBytes < 0)
+        {
+            perror("sendto failed");
+        }
+    }
+
+    void updateRobotState()
+    {
+        robotState_->setJointGroupPositions(armJointModelGroup_, currentJointValues_);
+        robotState_->setJointPositions("left_finger_joint", &currentLeftFingerJoint_);
+        robotState_->setJointPositions("right_finger_joint", &currentRightFingerJoint_);
+    }
+
+    void jointStatesCallback(const sensor_msgs::JointStateConstPtr &jointStatePtr)
+    {
+        if (jointStatesFlag_ == false)
+        {
+            jointStatesFlag_ = true;
+            return;
+        }
+        if (jointStatePtr->position.size() != 8)
+        {
+            ROS_ERROR("The size of the joint state is wrong");
+            return;
+        }
+        for (int i = 0; i < armJointNumber_; ++i)
+        {
+            currentJointValues_[i] = jointStatePtr->position[i];
+        }
+        currentLeftFingerJoint_ = jointStatePtr->position[6];
+        currentRightFingerJoint_ = jointStatePtr->position[7];
+
+        updateRobotState();
+        sendO3DELinksInfo();
+    }
+
+    vector<double> getGripperJointsFromWidth(double width)
+    {
+        if (width < minWidth_)
+        {
+            ROS_ERROR_STREAM("The widht must > 0, but the width specified is " << width);
+            width = minWidth_;
+        }
+        if (width > maxWidth_)
+        {
+            ROS_ERROR_STREAM("The widht must > 0, but the width specified is " << width);
+            width = maxWidth_;
+        }
+        return {-width / 2, -width / 2};
+    }
+    
+    void sendO3DELinksInfo()
+    {
+        std::string dataToSend;
+        for (auto link : linkWithGeos_)
+        {
+            O3DELinkInfo info = createO3DELinkInfo(link, robotState_->getFrameTransform(link));
+            std::string infoString = info.linkName + ":" +
+                                    std::to_string(info.x) + "," + std::to_string(info.y) + "," + std::to_string(info.z) + "," +
+                                    std::to_string(info.qw) + "," + std::to_string(info.qx) + "," + std::to_string(info.qy) + "," + std::to_string(info.qz);
+            dataToSend += infoString + " ";
+        }
+
+        sendDataToClients(dataToSend);
+    }
+
+    O3DELinkInfo createO3DELinkInfo(const string &linkName, const Eigen::Affine3d &matrix)
+    {
+        O3DELinkInfo linkInfo;
+        geometry_msgs::Pose pose;
+        tf2::convert(matrix, pose);
+        linkInfo.linkName = linkName;
+        linkInfo.x = pose.position.x;
+        linkInfo.y = pose.position.y;
+        linkInfo.z = pose.position.z;
+        linkInfo.qw = pose.orientation.w;
+        linkInfo.qx = pose.orientation.x;
+        linkInfo.qy = pose.orientation.y;
+        linkInfo.qz = pose.orientation.z;
+        return linkInfo;
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc, argv, "umi_moveit_controller");
+    ros::AsyncSpinner spinner(2);
+    spinner.start();
+    UmiMoveitController umiController;
+    ros::waitForShutdown();
+    return 0;
+}
